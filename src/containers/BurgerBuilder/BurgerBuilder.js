3b6b08ab76e0f30c4efd5e230f5f64a18404120c
SaroshKhawar496 <sarosh.496@gmail.com>
2019-01-17 23:13:21 -0500

    components, hoc, containers folder. Created Burger, Layout and BurgerBuilder subfolders

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import Aux from '../../hoc/Auxi'
+
+class BurgerBuilder extends Component {
+    
+    render(){
+
+        return(
+
+            <Aux>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </Aux>
+            
+        );
+    }
+}
+
+export default BurgerBuilder
---
+++ src/hoc/Auxi.js
@@ -0,0 +1,4 @@
+// returning the props.children, this is just a warpping element
+const aux = (props) => props.children
+
+export default aux;
