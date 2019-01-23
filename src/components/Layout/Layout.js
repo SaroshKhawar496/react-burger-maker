d78355565e60e28127658ceb12352236536e321e
SaroshKhawar496 <sarosh.496@gmail.com>
2019-01-22 21:56:28 -0500

    Side Drawer now closes on clicking backdrop. Changed Layout to a class component from fxn component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Auxi';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    // Aux is the wrapping root element
-    <Aux>
-        <div>
-            <Toolbar/>
-            <SideDrawer />
-        </div>
+class Layout extends Component{
+    state = {
+        showSideDrawer: true
+    }
 
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-)
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
 
-export default layout;
+    render (){
+        return (
+            <Aux>
+            <div>
+                <Toolbar/>
+                <SideDrawer open={this.state.showSideDrawer} 
+                closed={this.sideDrawerClosedHandler}/>
+            </div>
+    
+            <main className={classes.Content}>
+                {/*App.js calls Layout and passes BurgerBuilder as children  */}
+                {this.props.children}
+            </main>
+        </Aux>
+
+        )
+    }
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,23 +1,36 @@
 import React from "react";
 
-import Logo from '../../Logo/Logo'
-import NavigationItems from '../NavigationItems/NavigationItems'
-import classes from './SideDrawer.css'
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+import BackDrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxi';
 
 const sideDrawer = (props) => {
 
-    return(
-        <div className={classes.SideDrawer}>
-            
-            <div className={classes.Logo}>
-                <Logo />
-            </div>
-           
-            <nav>
-                <NavigationItems />
-            </nav>
+    let  attachedClasses = [classes.SideDrawer, classes.Close];
 
-        </div>
+    if (props.open){
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
+    return(
+        <Aux>
+
+            <BackDrop show={props.open} clicked={props.closed}/>
+
+            <div className={attachedClasses.join(' ')}>
+                
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+            
+                <nav>
+                    <NavigationItems />
+                </nav>
+
+            </div>
+        </Aux>
     );
 };
 
