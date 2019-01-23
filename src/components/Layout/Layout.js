6b3383f08636e4ad1a3a3ae3ca7a0a0adc89c566
SaroshKhawar496 <sarosh.496@gmail.com>
2019-01-22 21:11:43 -0500

    Added SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Aux from '../../hoc/Auxi';
 import classes from './Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar'
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const layout = (props) => (
     // Aux is the wrapping root element
     <Aux>
         <div>
             <Toolbar/>
+            <SideDrawer />
         </div>
 
         <main className={classes.Content}>
