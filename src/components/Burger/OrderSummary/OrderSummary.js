55e1dbf21d1bd9edece6ff56226d84341539de2b
SaroshKhawar496 <sarosh.496@gmail.com>
2019-01-22 22:34:16 -0500

    Added some component lifecyle hooks in Modal and Order Summary. To improve performance.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Aux from '../../../hoc/Auxi';
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-    // getting ingredients in a object format
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    // OrderSummary could be a fxnal component again.
+    // componentWillUpdate(){
+    //     console.log("OrderSummary will update");
+    // }
+
+    render(){
+            // getting ingredients in a object format
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey=>{
         return <li key={igKey}>
             <span style={{textTransform:'capitalize'}}>{igKey}</span>
-        : {props.ingredients[igKey]}</li>
+        : {this.props.ingredients[igKey]}</li>
     })
 
-    return(
-
-        <Aux>
+        return(
+            <Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: ${props.price.toFixed(2)} </strong></p>
+            <p><strong>Total Price: ${this.props.price.toFixed(2)} </strong></p>
             <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
 
         </Aux>
-    )
-};
+        )
+    }
+}
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.css';
 import Aux from '../../../hoc/Auxi'
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => {
-    return(
-        <Aux>
-            <Backdrop show={props.show} clicked={props.modalClosed}/>
-    <div 
-    className={classes.Modal}
-    style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh),',
-        opacity: props.show ? '1' : '0'
-    }}>
-        {props.children}
-    </div>
-    </Aux>
-    );
-};
+class Modal extends Component {
+    
+    // in BurgerBuilder.js, Modal wraps OrderSummary and if Modal
+    // doesn't update neither does the OrderSummary
+    shouldComponentUpdate(nextProps, nextState) {
+        // will render it again only if the show changes
+        if (nextProps.show !== this.props.show) {
+            return true;
+        }
+        return false;
+    }
+    componentWillUpdate() {
+        console.log("Modal will update.")
+    }
 
-export default modal;
+    render(){
+
+        return(
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div 
+                className={classes.Modal}
+                style={{
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh),',
+                    opacity: this.props.show ? '1' : '0'
+                }}>
+                    {this.props.children}
+                </div>
+         </Aux>
+        );
+    }
+}
+
+export default Modal;
