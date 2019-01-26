26eefc69e00f1a8f1580ce8a2080110378386890
SaroshKhawar496 <sarosh.496@gmail.com>
2019-01-26 00:58:10 -0500

    Added a Post Request to firebase on order now button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,7 +4,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
-
+import axios from '../../axios-orders'
 
 const INGRIDIENT_PRICES = {
     salad: 0.5,
@@ -92,7 +92,34 @@ class BurgerBuilder extends Component {
     }
     
     puchaseContinueHandler = () =>{
-        alert('You continue!');
+        // alert('You continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            // for real app you would re-calculate the price on the
+            // server. as the user might change it on front end and 
+            // submit!
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Sarosh',
+                address: {
+                    street: 'test street 1',
+                    zipCode: '123123',
+                    country: 'Canada'
+                },
+                email: 'test@gmail.com'
+            },
+            deliverMethod: 'fastest'
+        }
+        // sending post request to firebase
+        // for firebase, /orders will create a new node or route 
+        // .json is needed
+        axios.post('/orders.json',order).then(
+            (response)=>{
+                console.log(response);
+            }
+        ).catch((error)=>{
+            console.log(error);
+        });
     }
 
     render(){
