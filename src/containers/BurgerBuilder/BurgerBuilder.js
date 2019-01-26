15416f6c597fcb53a222e4e5fcc47b110f97d0de
SaroshKhawar496 <sarosh.496@gmail.com>
2019-01-26 01:40:08 -0500

    Wrapped BurgerBuilder with withErrorHandler hoc component.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,8 @@ import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 import axios from '../../axios-orders'
 import Spinner from '../../components/UI/Spinner/Spinner'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
+
 
 const INGRIDIENT_PRICES = {
     salad: 0.5,
@@ -169,4 +171,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder
+export default withErrorHandler(BurgerBuilder, axios)
