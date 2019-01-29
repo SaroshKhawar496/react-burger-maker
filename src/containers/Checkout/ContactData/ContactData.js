4a4208ea709fa92e769f83ba4f6f313015450936
SaroshKhawar496 <sarosh.496@gmail.com>
2019-01-28 23:05:19 -0500

    Added 2 way binding to ContactData form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -91,6 +91,19 @@ class ContactData extends Component {
         });
     }
 
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        }
+        // doing deep cloning/copy
+        const updatedFormElement= {
+            ... updatedOrderForm[inputIdentifier]
+        } 
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({orderForm: updatedOrderForm});
+    }
+
     render(){
 
         const formElementsArray = [];
@@ -108,7 +121,9 @@ class ContactData extends Component {
                  key={formElement.id}
                  elementType={formElement.config.elementType}
                  elementCofig= {formElement.config.elementCofig}
-                 value= {formElement.config.value}/>
+                 value= {formElement.config.value}
+                 changed={(event)=> this.inputChangedHandler(event, formElement.id)}
+                 />
             ))}
             
             <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
